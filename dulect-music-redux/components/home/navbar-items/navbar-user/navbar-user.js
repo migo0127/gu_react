29e75c3ffa9c44ab18a28cb2afcfd8394b85e691
migo0127 <61925863+migo0127@users.noreply.github.com>
2020-06-16 18:57:00 +0800

    20200616

---
+++ dulect-music-redux/components/home/navbar-items/navbar-user/navbar-user.js
@@ -0,0 +1,61 @@
+import React,{useState, Component} from 'react';
+import PropTypes from 'prop-types';
+import { render } from 'react-dom';
+
+class NavbarUser extends Component{
+
+    state = {
+        login:true,
+        width:window.innerWidth
+      }
+
+      static propTypes = {
+        homeUser:PropTypes.object.isRequired
+    }
+
+    componentDidMount(){
+        const {width} = this.state;
+
+        const handleRWD = ()=>{
+            const width = window.innerWidth;
+            this.setState({width});
+            // console.log(width);
+
+        }
+
+        window.addEventListener('resize',handleRWD);
+    }
+
+    render(){
+        const display = this.state.login ? 'none':'block';
+        const display2 = this.state.login ? 'block':'none';
+        const display3 = this.state.login && this.state.width > 768 ? '':'none';
+        const {homeUser} = this.props;
+
+        return (
+        <>
+            <a href="javascript:;" style={{display}}>
+            <i className="fas fa-user"></i>
+                </a>
+            <p className="home-username" style={{display:display2}}>{homeUser.username}</p>
+            <ul className="home-nav-userlogin" style={{display:display3}}>
+                <li>
+                    <a src="javascript:;">我的帳戶</a>
+                </li>
+                <li>
+                    <a src="javascript:;">我的影片</a>
+                </li>
+                <li>
+                    <a src="javascript:;">購買清單</a>
+                </li>
+                <li>
+                    <a src="javascript:;">登出</a>
+                </li>
+            </ul>
+        </>
+
+        )
+    }
+}
+
+export default NavbarUser;
